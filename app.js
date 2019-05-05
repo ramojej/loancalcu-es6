8feaeb8d8f0ff0db59d403a9c35dd879eaed00a8
Jejo <jejomar@im.skubbs.com>
2019-05-05 23:13:45 +0800

    Added some user experience

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 //Listen for submit
 
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+    //hide results
+    document.getElementById('results').style.display = 'none';
+    //show loader
+    document.getElementById('loading').style.display = 'block';
 
-function calculateResults(e) {
+    setTimeout(calculateResults, 1000);
+
+    e.preventDefault();
+});
+
+function calculateResults() {
     //UI vars
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
@@ -23,15 +32,21 @@ function calculateResults(e) {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+        //show results
+        document.getElementById('results').style.display = 'block';
+        //hide loader
+        document.getElementById('loading').style.display = 'none';
     } else {
         showError('Please check your numbers');
     }
-
-    e.preventDefault();
 }
 
 //Show Error
 function showError(error) {
+    //hide results
+    document.getElementById('results').style.display = 'none';
+    //hide loader
+    document.getElementById('loading').style.display = 'none';
     //create a div
     const errorDiv = document.createElement('div');
 
